6bd555cff15acc6accafea06d030147e91dbec0a
Osama Hamad <osamahama2005@gmail.com>
2021-02-02 18:40:42 -0500

    initial commit

---
+++ lines.py
@@ -0,0 +1,8 @@
+class Line:
+    def __init__(self, coord1, coord2):
+        self.coord1 = coord1
+        self.coord2 = coord2
+        self.filled = False
+
+    def fill_line(self):
+        self.line == True
---
+++ squares.py
@@ -0,0 +1,3 @@
+class Square:
+    def __init__(self, dot1):
+        self.dot1 = dot1
